eb20ea1786be0893bf67c327880e266364d62825
Louis Tan <louistan600@gmail.com>
2026-03-13 21:56:21 +0800

    update access to public

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ namespace DotnetCrudAuthApi.Models
     {
         public int UserId { get; set; }
         public string Email { get; set; } = "";
-        byte[] PasswordHash { get; set; } = new byte[0];
-        byte[] PasswordSalt { get; set; } = new byte[0];
+        public byte[] PasswordHash { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = new byte[0];
     }
 }
