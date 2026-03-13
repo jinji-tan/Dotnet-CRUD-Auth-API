98e212face76106e963e415ce6e6fdede8523e17
Louis Tan <louistan600@gmail.com>
2026-03-13 18:30:13 +0800

    add model

---
+++ Models/People.cs
@@ -0,0 +1,11 @@
+namespace DotnetCrudAuthApi.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+        public string Gender { get; set; } = "";
+    }
+}
