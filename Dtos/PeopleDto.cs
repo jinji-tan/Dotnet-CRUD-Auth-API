187afa39069a81f70cd215ea3f2d675d8a35ae6d
Louis Tan <louistan600@gmail.com>
2026-03-13 18:32:10 +0800

    added peopledto

---
+++ Dtos/PeopleDto.cs
@@ -0,0 +1,10 @@
+namespace DotnetCrudAuthApi.Dtos
+{
+    public class PeopleDto
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+        public string Gender { get; set; } = "";
+    }
+}
